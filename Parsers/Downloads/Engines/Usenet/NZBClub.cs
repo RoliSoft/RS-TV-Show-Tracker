7e61232d0b823f679252cdebed20081c52c2ad3a
RoliSoft <rolisoft@gmail.com>
2011-02-12 01:44:23 +0200

    Added NZBClub support.

---
+++ Parsers/Downloads/Engines/Usenet/NZBClub.cs
@@ -0,0 +1,107 @@
+﻿namespace RoliSoft.TVShowTracker.Parsers.Downloads.Engines.Usenet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    using HtmlAgilityPack;
+
+    using NUnit.Framework;
+
+    using RoliSoft.TVShowTracker.Parsers.Downloads.Engines.Torrent;
+
+    /// <summary>
+    /// Provides support for scraping NZBClub.
+    /// </summary>
+    [Parser("RoliSoft", "2011-02-12 1:23 AM"), TestFixture]
+    public class NZBClub : DownloadSearchEngine
+    {
+        /// <summary>
+        /// Gets the name of the site.
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get
+            {
+                return "NZBClub";
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL of the site.
+        /// </summary>
+        /// <value>The site location.</value>
+        public override string Site
+        {
+            get
+            {
+                return "http://nzbclub.com/";
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL to the favicon of the site.
+        /// </summary>
+        /// <value>The icon location.</value>
+        public override string Icon
+        {
+            get
+            {
+                return "http://nzbclub.com/images/favicon.ico";
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the site requires authentication.
+        /// </summary>
+        /// <value><c>true</c> if requires authentication; otherwise, <c>false</c>.</value>
+        public override bool Private
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the link.
+        /// </summary>
+        /// <value>The type of the link.</value>
+        public override Types Type
+        {
+            get
+            {
+                return Types.Usenet;
+            }
+        }
+
+        /// <summary>
+        /// Searches for download links on the service.
+        /// </summary>
+        /// <param name="query">The name of the release to search for.</param>
+        /// <returns>List of found download links.</returns>
+        public override IEnumerable<Link> Search(string query)
+        {
+            var html  = Utils.GetHTML(Site + "search.aspx?q=" + Uri.EscapeUriString(query));
+            var links = html.DocumentNode.SelectNodes("//span[contains(@id, 'SubjectLabel')]/a[1]");
+
+            if (links == null)
+            {
+                yield break;
+            }
+
+            foreach (var node in links)
+            {
+                var link = new Link(this);
+
+                link.Release = HtmlEntity.DeEntitize(node.InnerText);
+                link.URL     = Site.TrimEnd('/') + node.GetNodeAttributeValue("../../..//span[contains(@id, 'sizelabel')]/a", "href");
+                link.Size    = Regex.Match(node.GetHtmlValue("../../..//span[contains(@id, 'sizecolumnlabel')]"), @"^(?:<b>)?([^<]+)").Groups[1].Value;
+                link.Quality = ThePirateBay.ParseQuality(link.Release.Replace(' ', '.'));
+
+                yield return link;
+            }
+        }
+    }
+}
