9619108fc215ef841d02cb625094c61c60c69ef0
RoliSoft <rolisoft@gmail.com>
2011-02-12 04:08:30 +0200

    Added PreScene support.

---
+++ Parsers/Downloads/Engines/PreDB/PreScene.cs
@@ -0,0 +1,117 @@
+﻿namespace RoliSoft.TVShowTracker.Parsers.Downloads.Engines.PreDB
+{
+    using System;
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    using RoliSoft.TVShowTracker.Downloaders;
+    using RoliSoft.TVShowTracker.Parsers.Downloads.Engines.Torrent;
+
+    /// <summary>
+    /// Provides support for scraping ReleaseLog.
+    /// </summary>
+    [Parser("RoliSoft", "2011-02-12 3:58 AM"), TestFixture]
+    public class PreScene : DownloadSearchEngine
+    {
+        /// <summary>
+        /// Gets the name of the site.
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get
+            {
+                return "PreScene";
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL of the site.
+        /// </summary>
+        /// <value>The site location.</value>
+        public override string Site
+        {
+            get
+            {
+                return "http://prescene.com/";
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL to the favicon of the site.
+        /// </summary>
+        /// <value>The icon location.</value>
+        public override string Icon
+        {
+            get
+            {
+                return "http://prescene.com/favicon.ico";
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the site requires authentication.
+        /// </summary>
+        /// <value><c>true</c> if requires authentication; otherwise, <c>false</c>.</value>
+        public override bool Private
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the link.
+        /// </summary>
+        /// <value>The type of the link.</value>
+        public override Types Type
+        {
+            get
+            {
+                return Types.PreDB;
+            }
+        }
+
+        /// <summary>
+        /// Returns an <c>IDownloader</c> object which can be used to download the URLs provided by this parser.
+        /// </summary>
+        /// <value>The downloader.</value>
+        public override IDownloader Downloader
+        {
+            get
+            {
+                return new ExternalDownloader();
+            }
+        }
+
+        /// <summary>
+        /// Searches for download links on the service.
+        /// </summary>
+        /// <param name="query">The name of the release to search for.</param>
+        /// <returns>List of found download links.</returns>
+        public override IEnumerable<Link> Search(string query)
+        {
+            var html  = Utils.GetHTML(Site, "q=" + Uri.EscapeUriString(query));
+            var links = html.DocumentNode.SelectNodes("//h4/a");
+
+            if (links == null)
+            {
+                yield break;
+            }
+
+            foreach (var node in links)
+            {
+                var link = new Link(this);
+
+                link.Release = node.InnerText;
+                link.URL     = Site.TrimEnd('/') + node.GetAttributeValue("href");
+                link.Size    = "N/A";
+                link.Quality = ThePirateBay.ParseQuality(link.Release);
+
+                yield return link;
+            }
+        }
+    }
+}
