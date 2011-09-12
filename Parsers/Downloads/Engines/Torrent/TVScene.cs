79e67bb3e96edc8f339b5c0951f7caf4c9913206
RoliSoft <rolisoft@gmail.com>
2011-09-12 17:32:01 +0300

    Added support for TV Chaos UK and its sister site, TV-Scene.

---
+++ Parsers/Downloads/Engines/Torrent/TVScene.cs
@@ -0,0 +1,169 @@
+﻿namespace RoliSoft.TVShowTracker.Parsers.Downloads.Engines.Torrent
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Authentication;
+    using System.Text.RegularExpressions;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Provides support for scraping TV-Scene.
+    /// </summary>
+    [Parser("2011-09-12 17:25 PM"), TestFixture]
+    public class TVScene : DownloadSearchEngine
+    {
+        /// <summary>
+        /// Gets the name of the site.
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get
+            {
+                return "TV-Scene";
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL of the site.
+        /// </summary>
+        /// <value>The site location.</value>
+        public override string Site
+        {
+            get
+            {
+                return "http://tv-scene.com/";
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the site requires authentication.
+        /// </summary>
+        /// <value><c>true</c> if requires authentication; otherwise, <c>false</c>.</value>
+        public override bool Private
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Gets the names of the required cookies for the authentication.
+        /// </summary>
+        /// <value>The required cookies for authentication.</value>
+        public override string[] RequiredCookies
+        {
+            get
+            {
+                return new[] { "c_secure_uid", "c_secure_pass" };
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this search engine can login using a username and password.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if this search engine can login; otherwise, <c>false</c>.
+        /// </value>
+        public override bool CanLogin
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL to the login page.
+        /// </summary>
+        /// <value>The URL to the login page.</value>
+        public override string LoginURL
+        {
+            get
+            {
+                return Site + "takelogin.php";
+            }
+        }
+
+        /// <summary>
+        /// Gets the input fields of the login form.
+        /// </summary>
+        /// <value>The input fields of the login form.</value>
+        public override Dictionary<string, object> LoginFields
+        {
+            get
+            {
+                return new Dictionary<string, object>
+                    {
+                        { "username", LoginFieldTypes.UserName },
+                        { "password", LoginFieldTypes.Password },
+                    };
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the link.
+        /// </summary>
+        /// <value>The type of the link.</value>
+        public override Types Type
+        {
+            get
+            {
+                return Types.Torrent;
+            }
+        }
+
+        /// <summary>
+        /// Searches for download links on the service.
+        /// </summary>
+        /// <param name="query">The name of the release to search for.</param>
+        /// <returns>List of found download links.</returns>
+        public override IEnumerable<Link> Search(string query)
+        {
+            var html = Utils.GetHTML(Site + "browse.php", "do=search&search_type=t_name&category=0&include_dead_torrents=yes&keywords=" + Uri.EscapeUriString(query), cookies: Cookies);
+
+            if (GazelleTrackerLoginRequired(html.DocumentNode))
+            {
+                throw new InvalidCredentialException();
+            }
+
+            var links = html.DocumentNode.SelectNodes("//div[@class='tooltip-target']/a");
+
+            if (links == null)
+            {
+                yield break;
+            }
+
+            foreach (var node in links)
+            {
+                var link = new Link(this);
+
+                link.Release = node.GetTextValue("../../div[2]/div[1]").Trim();
+                link.InfoURL = node.GetAttributeValue("href");
+                link.FileURL = node.GetNodeAttributeValue("../../../td[3]/a", "href");
+                link.Size    = node.GetHtmlValue("../../../td[5]").Trim();
+                link.Quality = Regex.IsMatch(link.Release, @"\b(HD|720p|1080[ip]|[xh]\.?264)\b")
+                               ? Qualities.HDTV720p
+                               : Qualities.HDTVXviD;
+                link.Infos   = Link.SeedLeechFormat.FormatWith(node.GetTextValue("../../../td[7]").Trim(), node.GetTextValue("../../../td[8]").Trim())
+                             + (node.GetHtmlValue("../../div/span/img[starts-with(@title, \"Fast Speed\")]") != null ? " / " + Regex.Match(node.GetNodeAttributeValue("../../div/span/img[starts-with(@title, \"Fast Speed\")]", "title"), @"has (\d+)").Groups[1].Value + " seedbox" : string.Empty)
+                             + (node.GetHtmlValue("../../div/span/img[starts-with(@title, \"Free Torrent\")]") != null ? ", Free" : string.Empty);
+
+                yield return link;
+            }
+        }
+
+        /// <summary>
+        /// Authenticates with the site and returns the cookies.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>Cookies on success, <c>string.Empty</c> on failure.</returns>
+        public override string Login(string username, string password)
+        {
+            return GazelleTrackerLogin(username, password);
+        }
+    }
+}
