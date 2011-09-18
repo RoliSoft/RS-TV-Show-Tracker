484e485c1aa1894a9f9aaac5aa13b55653ed2add
RoliSoft <rolisoft@gmail.com>
2011-09-18 23:54:51 +0300

    Added support for NyaaTorrents and Tokyo Toshokan as requested on reddit.

---
+++ Parsers/Downloads/Engines/Torrent/NyaaTorrents.cs
@@ -0,0 +1,107 @@
+﻿namespace RoliSoft.TVShowTracker.Parsers.Downloads.Engines.Torrent
+{
+    using System;
+    using System.Collections.Generic;
+
+    using HtmlAgilityPack;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Provides support for scraping NyaaTorrents.
+    /// </summary>
+    [Parser("2011-09-18 11:22 AM"), TestFixture]
+    public class NyaaTorrents : DownloadSearchEngine
+    {
+        /// <summary>
+        /// Gets the name of the site.
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get
+            {
+                return "NyaaTorrents";
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL of the site.
+        /// </summary>
+        /// <value>The site location.</value>
+        public override string Site
+        {
+            get
+            {
+                return "http://www.nyaa.eu/";
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL to the favicon of the site.
+        /// </summary>
+        /// <value>The icon location.</value>
+        public override string Icon
+        {
+            get
+            {
+                return "http://files.nyaa.eu/nt";
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the link.
+        /// </summary>
+        /// <value>The type of the link.</value>
+        public override Types Type
+        {
+            get
+            {
+                return Types.Torrent;
+            }
+        }
+
+        /// <summary>
+        /// Searches for download links on the service.
+        /// </summary>
+        /// <param name="query">The name of the release to search for.</param>
+        /// <returns>List of found download links.</returns>
+        public override IEnumerable<Link> Search(string query)
+        {
+            var html  = Utils.GetHTML(Site + "?page=search&cats=1_0&filter=0&term=" + Uri.EscapeUriString(ShowNames.Parser.ReplaceEpisode(query, "{1}", false, false)));
+            var links = html.DocumentNode.SelectNodes("//table/tr/td[@class='tlistname']/a");
+
+            if (links == null)
+            {
+                yield break;
+            }
+
+            foreach (var node in links)
+            {
+                var link = new Link(this);
+
+                link.Release = HtmlEntity.DeEntitize(node.InnerText);
+                link.InfoURL = HtmlEntity.DeEntitize(node.GetAttributeValue("href"));
+                link.FileURL = HtmlEntity.DeEntitize(node.GetNodeAttributeValue("../../td[@class='tlistdownload']/a", "href"));
+                link.Size    = node.GetTextValue("../../td[@class='tlistsize']").Trim().Replace("i", string.Empty);
+                link.Quality = FileNames.Parser.ParseQuality(link.Release);
+
+                var tlistf = node.GetTextValue("../../td[@class='tlistfailed']");
+
+                if (tlistf == null)
+                {
+                    link.Infos = Link.SeedLeechFormat.FormatWith(node.GetTextValue("../../td[@class='tlistsn']").Trim(), node.GetTextValue("../../td[@class='tlistln']").Trim())
+                                 + (node.GetNodeAttributeValue("../..", "class").Contains("aplus") ? ", A+" : string.Empty)
+                                 + (node.GetNodeAttributeValue("../..", "class").Contains("trusted") ? ", Trusted" : string.Empty)
+                                 + (node.GetNodeAttributeValue("../..", "class").Contains("remake") ? ", Remake" : string.Empty);
+                }
+                else
+                {
+                    link.Infos = tlistf;
+                }
+
+                yield return link;
+            }
+        }
+    }
+}
---
+++ Parsers/Downloads/Engines/Torrent/TokyoToshokan.cs
@@ -0,0 +1,91 @@
+﻿namespace RoliSoft.TVShowTracker.Parsers.Downloads.Engines.Torrent
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    using HtmlAgilityPack;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Provides support for scraping Tokyo Toshokan.
+    /// </summary>
+    [Parser("2011-09-18 11:31 AM"), TestFixture]
+    public class TokyoToshokan : DownloadSearchEngine
+    {
+        /// <summary>
+        /// Gets the name of the site.
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get
+            {
+                return "Tokyo Toshokan";
+            }
+        }
+
+        /// <summary>
+        /// Gets the URL of the site.
+        /// </summary>
+        /// <value>The site location.</value>
+        public override string Site
+        {
+            get
+            {
+                return "http://tokyotosho.info/";
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the link.
+        /// </summary>
+        /// <value>The type of the link.</value>
+        public override Types Type
+        {
+            get
+            {
+                return Types.Torrent;
+            }
+        }
+
+        /// <summary>
+        /// Searches for download links on the service.
+        /// </summary>
+        /// <param name="query">The name of the release to search for.</param>
+        /// <returns>List of found download links.</returns>
+        public override IEnumerable<Link> Search(string query)
+        {
+            var html  = Utils.GetHTML(Site + "search.php?type=1&terms=" + Uri.EscapeUriString(ShowNames.Parser.ReplaceEpisode(query, "{1}", false, false)));
+            var links = html.DocumentNode.SelectNodes("//table/tr/td[@class='desc-top']/a");
+
+            if (links == null)
+            {
+                yield break;
+            }
+
+            foreach (var node in links)
+            {
+                var link = new Link(this);
+
+                link.Release = HtmlEntity.DeEntitize(node.InnerText);
+                link.InfoURL = Site + HtmlEntity.DeEntitize(node.GetNodeAttributeValue("../../td[@class='web']/a[last()]", "href"));
+                link.FileURL = HtmlEntity.DeEntitize(node.GetAttributeValue("href"));
+                link.Quality = FileNames.Parser.ParseQuality(link.Release);
+
+                var info = HtmlEntity.DeEntitize(node.GetTextValue("../../following-sibling::tr[1]"));
+                var size = Regex.Match(info, @"Size: (\d+(?:\.\d+)?)([KMG]B)");
+                
+                if (size.Success)
+                {
+                    link.Size = size.Groups[1].Value + " " + size.Groups[2].Value;
+                }
+
+                link.Infos = Link.SeedLeechFormat.FormatWith(Regex.Match(info, @"S: (\d+) ").Groups[1].Value, Regex.Match(info, @"L: (\d+) ").Groups[1].Value);
+
+                yield return link;
+            }
+        }
+    }
+}
