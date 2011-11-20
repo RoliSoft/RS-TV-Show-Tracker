c2533525968ae39bc63a938fb1a24c7f53233f79
RoliSoft <rolisoft@gmail.com>
2011-11-20 23:43:42 +0200

    Increased version number to 2.1.*, due to the introduction of plugins and the auto-downloader.

--- Signature.cs
+++ Signature.cs
@@ -14,25 +14,25 @@
         /// Gets the name of the software.
         /// </summary>
         /// <value>The software name.</value>
-        public static string Software { get; internal set; }
+        public static string Software { get; private set; }
 
         /// <summary>
         /// Gets the version number of the executing assembly.
         /// </summary>
         /// <value>The software version.</value>
-        public static string Version { get; internal set; }
+        public static string Version { get; private set; }
 
         /// <summary>
         /// Gets the date and time when the executing assembly was compiled.
         /// </summary>
         /// <value>The compile time.</value>
-        public static DateTime CompileTime { get; internal set; }
+        public static DateTime CompileTime { get; private set; }
 
         /// <summary>
         /// Gets the full path to the executing assembly.
         /// </summary>
         /// <value>The full path.</value>
-        public static string FullPath { get; internal set; }
+        public static string FullPath { get; private set; }
 
         /// <summary>
         /// This number is used for various purposes where a non-random unique number is required.
@@ -53,8 +53,8 @@
             var ver = Assembly.GetExecutingAssembly().GetName().Version;
 
             Software       = "RS TV Show Tracker";
-            Version        = ver.Major + "." + ver.MajorRevision + "." + ver.Build.ToString("0000") + "." + ver.Revision.ToString("00000");
-            CompileTime    = new DateTime(2000, 1, 1, 1, 0, 0).AddDays(ver.Build).AddSeconds(ver.Revision * 2);
+            Version        = ver.Major + "." + ver.Minor + "." + ver.Build.ToString("0000") + "." + ver.Revision.ToString("00000");
+            CompileTime    = Utils.VersionToDateTime(ver);
             try { FullPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + Path.DirectorySeparatorChar; } catch (ArgumentException) { }
         }
 
