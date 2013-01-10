6c34ea982cd271eefc31cfa295c13c92e1ce9e77
RoliSoft <rolisoft@gmail.com>
2013-01-10 18:40:31 +0200

    Ported OnlineVideoSearchEngineTaskDialog. That's one fucking long class name. I'm proud of it.

--- TaskDialogs/OnlineVideoSearchEngineTaskDialog.cs
+++ TaskDialogs/OnlineVideoSearchEngineTaskDialog.cs
@@ -5,18 +5,16 @@
 
     using Microsoft.WindowsAPICodePack.Taskbar;
 
+    using TaskDialogInterop;
+
     using RoliSoft.TVShowTracker.Parsers.OnlineVideos;
     using RoliSoft.TVShowTracker.Parsers.Guides;
-
-    using VistaControls.TaskDialog;
 
     /// <summary>
     /// Provides a <c>TaskDialog</c> frontend to the <c>OnlineVideoSearchEngine</c> class.
     /// </summary>
     public class OnlineVideoSearchEngineTaskDialog
     {
-        private TaskDialog _td;
-        private Result _res;
         private OnlineVideoSearchEngine _os;
         private volatile bool _active;
 
@@ -38,53 +36,52 @@
         /// <param name="ep">The episode.</param>
         public void Search(Episode ep)
         {
-            _td = new TaskDialog
-                {
-                    Title           = "Searching...",
-                    Instruction     = "{0} S{1:00}E{2:00}".FormatWith(ep.Show.Name, ep.Season, ep.Number),
-                    Content         = "Searching for the episode...",
-                    CommonButtons   = TaskDialogButton.Cancel,
-                    ShowProgressBar = true
-                };
-
-            _td.SetMarqueeProgressBar(true);
-            _td.Destroyed   += TaskDialogDestroyed;
-            _td.ButtonClick += TaskDialogDestroyed;
-            
             _active = true;
-            new Thread(() =>
+            var showmbp = false;
+            var mthd = new Thread(() => TaskDialog.Show(new TaskDialogOptions
                 {
-                    Thread.Sleep(500);
+                    Title                   = "Searching...",
+                    MainInstruction         = "{0} S{1:00}E{2:00}".FormatWith(ep.Show.Name, ep.Season, ep.Number),
+                    Content                 = "Searching for the episode...",
+                    CustomButtons           = new[] { "Cancel" },
+                    ShowMarqueeProgressBar  = true,
+                    EnableCallbackTimer     = true,
+                    AllowDialogCancellation = true,
+                    Callback                = (dialog, args, data) =>
+                        {
+                            if (!showmbp)
+                            {
+                                dialog.SetProgressBarMarquee(true, 0);
+                                showmbp = true;
+                            }
 
-                    if (_active)
-                    {
-                        _res = _td.Show().CommonButton;
-                    }
-                }).Start();
+                            if (args.ButtonId != 0)
+                            {
+                                if (_active)
+                                {
+                                    try { _os.CancelSearch(); } catch { }
+                                }
+
+                                return false;
+                            }
+
+                            if (!_active)
+                            {
+                                dialog.ClickButton(500);
+                                return false;
+                            }
+
+                            return true;
+                        }
+                }));
+            mthd.SetApartmentState(ApartmentState.STA);
+            mthd.Start();
 
             _os.SearchAsync(ep);
 
             Utils.Win7Taskbar(state: TaskbarProgressBarState.Indeterminate);
         }
 
-        /// <summary>
-        /// Handles the Destroyed event of the _td control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
-        private void TaskDialogDestroyed(object sender, EventArgs e)
-        {
-            if (_active && (_res == Result.Cancel || (e is ClickEventArgs && (e as ClickEventArgs).ButtonID == 2)))
-            {
-                Utils.Win7Taskbar(state: TaskbarProgressBarState.NoProgress);
-
-                _active = false;
-                _res    = Result.Cancel;
-
-                _os.CancelSearch();
-            }
-        }
-        
         /// <summary>
         /// Called when the online search is done.
         /// </summary>
@@ -95,16 +92,6 @@
             _active = false;
 
             Utils.Win7Taskbar(state: TaskbarProgressBarState.NoProgress);
-
-            if (_td != null && _td.IsShowing)
-            {
-                _td.SimulateButtonClick(-1);
-            }
-            
-            if (_res == Result.Cancel)
-            {
-                return;
-            }
 
             Utils.Run(e.Second);
         }
@@ -119,45 +106,35 @@
             _active = false;
 
             Utils.Win7Taskbar(state: TaskbarProgressBarState.NoProgress);
-
-            if (_td != null && _td.IsShowing)
-            {
-                _td.SimulateButtonClick(-1);
-            }
-
-            if (_res == Result.Cancel)
-            {
-                return;
-            }
-
-            var nvftd = new TaskDialog
+            
+            var nvftd = new TaskDialogOptions
                 {
-                    CommonIcon    = TaskDialogIcon.Stop,
-                    Title         = "No videos found",
-                    Instruction   = e.First,
-                    Content       = e.Second,
-                    CommonButtons = TaskDialogButton.OK
+                    MainIcon        = VistaTaskDialogIcon.Error,
+                    Title           = "No videos found",
+                    MainInstruction = e.First,
+                    Content         = e.Second
                 };
 
             if (!string.IsNullOrWhiteSpace(e.Third.Item3))
             {
-                nvftd.ExpandedInformation = e.Third.Item3;
+                nvftd.ExpandedInfo = e.Third.Item3;
             }
 
             if (!string.IsNullOrEmpty(e.Third.Item1))
             {
-                nvftd.UseCommandLinks = true;
-                nvftd.CustomButtons   = new[] { new CustomButton(0, e.Third.Item1) };
-                nvftd.ButtonClick    += (s, c) =>
-                    {
-                        if (c.ButtonID == 0)
-                        {
-                            Utils.Run(e.Third.Item2);
-                        }
-                    };
+                nvftd.CommandButtons = new[] { e.Third.Item1, "Close" };
+            }
+            else
+            {
+                nvftd.CustomButtons = new[] { "OK" };
             }
 
-            nvftd.Show();
+            var res = TaskDialog.Show(nvftd);
+
+            if (res.CommandButtonResult.HasValue && res.CommandButtonResult.Value == 0)
+            {
+                Utils.Run(e.Third.Item2);
+            }
         }
     }
 }
